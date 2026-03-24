7fc86265dcd632d74796c7c30334620d8ecaf096
Pedro Lucas <156856961+pedrolcsdev@users.noreply.github.com>
2026-03-23 21:16:03 -0300

    removendo bun.lock

--- .next/static/webpack/webpack.6c643dbf81373b3d.hot-update.js
+++ .next/static/webpack/webpack.6c643dbf81373b3d.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "741a48c203148dad"; }
+/******/ 	__webpack_require__.h = function() { return "f67ae9bb5fed6547"; }
 /******/ }();
 /******/ 
 /******/ }
